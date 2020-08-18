a969b4b86406fc6d8282d4de98aceb0d43d02773
Kodie Grantham <kodie.grantham@gmail.com>
2020-08-18 11:11:13 -0500

    Reupload images to imgur and log imageCount in console log

--- images.js
+++ images.js
@@ -1,97 +1,187 @@
 var images = [
   {
-    "url": "https://uced93063d60228a7890bd296618.dl.dropboxusercontent.com/cd/0/inline/A9pCn0BXO2AotfN8AO4X3KFpFx1eCZRlAp4A5jCEquas9muV8Nabicb35kI5E8RwDp0NUgXQSF5W98fbaTzc_uOw0IxSgeOQLLy36s-qIw4rYyxqw_fIPILoyDRRToncSD8/file#",
+    "url": "https://i.imgur.com/qT9DB4N.jpg",
+    "width": 1080,
+    "height": 1080
+  },
+  {
+    "url": "https://i.imgur.com/ZtPVKst.jpg",
+    "width": 640,
+    "height": 640
+  },
+  {
+    "url": "https://i.imgur.com/2fIH1Xn.jpg",
+    "width": 1200,
+    "height": 1600
+  },
+  {
+    "url": "https://i.imgur.com/f14jAVN.jpg",
+    "width": 1200,
+    "height": 1600
+  },
+  {
+    "url": "https://i.imgur.com/N9VW2kV.jpg",
+    "width": 1600,
+    "height": 1200
+  },
+  {
+    "url": "https://i.imgur.com/EOfjO1c.jpg",
+    "width": 1200,
+    "height": 1600
+  },
+  {
+    "url": "https://i.imgur.com/OY0QvnC.jpg",
+    "width": 2448,
+    "height": 3264
+  },
+  {
+    "url": "https://i.imgur.com/Ip97RAF.jpg",
+    "width": 2988,
+    "height": 5312
+  },
+  {
+    "url": "https://i.imgur.com/FIHzaD9.jpg",
+    "width": 1836,
+    "height": 3264
+  },
+  {
+    "url": "https://i.imgur.com/551qfzI.jpg",
+    "width": 3933,
+    "height": 2818
+  },
+  {
+    "url": "https://i.imgur.com/q5OX2sA.jpg",
+    "width": 1456,
+    "height": 2592
+  },
+  {
+    "url": "https://i.imgur.com/hY9NELg.jpg",
+    "width": 873,
+    "height": 873
+  },
+  {
+    "url": "https://i.imgur.com/7igTKOf.jpg",
     "width": 722,
     "height": 1280
   },
   {
-    "url": "https://uc8bf609369c92bde47a1c406251.dl.dropboxusercontent.com/cd/0/inline/A9oX2supCRLv4q_5bh5yXwIHrN8dDG6DfFSgrGUYxm64WfneqIJIkjAsl3sSS58TanYXRNUvj8JjcZSR31uDDF3KuAiZmkkUyhfoVIk5QkE7yj5mUazaJgsVQv6fuLoQLvE/file#",
-    "width": 692,
-    "height": 692
+    "url": "https://i.imgur.com/QA1Ssqp.jpg",
+    "width": 722,
+    "height": 1280
   },
   {
-    "url": "https://uc1a93e0f231287ffb7f6bc5d4b9.dl.dropboxusercontent.com/cd/0/inline/A9oK9GaWo2QNnpha-y2QQ2oDM4POXJsUCL_8AXPR3yExWCxeqe5df6sxEpL0t2X7D_cKzjEQDNpxpc8n8zyQ9Fw_8ss4z8JbyaTRFGGx0GrLYczF42_a3XuFFk3o5mhCnNc/file#",
-    "width": 519,
-    "height": 692
+    "url": "https://i.imgur.com/ij1dLCD.jpg",
+    "width": 464,
+    "height": 1069
   },
   {
-    "url": "https://uc9a2ccbd093c56289554bd340c6.dl.dropboxusercontent.com/cd/0/inline/A9oeyxXF1i0pi1JHIqUtZMmPYs8DYeHnGOyt9Dr0NRRSifm8eKAEDv2iVLUk5k9n0A-JsO0fO_LPXeWUi4QEvuAVF6psE9-x1sqheFxzMRoUHxdxuY4s5eYloLzgw7vTG4g/file#",
-    "width": 519,
-    "height": 692
+    "url": "https://i.imgur.com/yTFTOmr.jpg",
+    "width": 623,
+    "height": 1279
   },
   {
-    "url": "https://uc0705c985a361d7737635bbef77.dl.dropboxusercontent.com/cd/0/inline/A9o5Mrs2MrSazPUKh9A5Kr6N6RPUInLxFt80wQxdZs8HblnTKJKqKfBNuPnu-bTU4tdrkDMnrqLuNLJzI3HHNozv6peGWRTH1kbt_vo3PCyHYcnke3V-92vZ3P-ipQ0w0_g/file#",
-    "width": 389,
-    "height": 692
+    "url": "https://i.imgur.com/3OoVgfd.jpg",
+    "width": 720,
+    "height": 1280
   },
   {
-    "url": "https://uc6c5354a135f17c44702894dea3.dl.dropboxusercontent.com/cd/0/inline/A9pcecf-KEMq8IoaRu5hFkQ_Szcea1XBrLhmej4AsbMnmEN99HB9SDQ2YHYcHDWmrAuGecn9E50htshhmzm-4WczrJD7ek05mgRJZDarSjdca_pmqfwvGJzpOmAail0JFb8/file#",
-    "width": 390,
-    "height": 692
+    "url": "https://i.imgur.com/O9mDxGA.jpg",
+    "width": 1080,
+    "height": 1920
   },
   {
-    "url": "https://ucfc09c1705ab933e5c58e9d7462.dl.dropboxusercontent.com/cd/0/inline/A9qjqEKfAEw0tpfcPk8D-qpbuTnJhulJOiOWxRod_whKWS4qb1DWyL722kX0wHDEeZtPb7JVy0nG2-Cu4G-cwOrBrlo-5QUphyD74lenm8vC9uCIFiCtFCrht4NbtTlUQvw/file#",
+    "url": "https://i.imgur.com/g2eZb1x.jpg",
+    "width": 1920,
+    "height": 2560
+  },
+  {
+    "url": "https://i.imgur.com/G6TbwIO.jpg",
+    "width": 1512,
+    "height": 1512
+  },
+  {
+    "url": "https://i.imgur.com/AqxbipF.jpg",
+    "width": 3024,
+    "height": 4032
+  },
+  {
+    "url": "https://i.imgur.com/QG1F7YX.jpg",
+    "width": 1440,
+    "height": 2712
+  },
+  {
+    "url": "https://i.imgur.com/VNRfQ2o.jpg",
+    "width": 3024,
+    "height": 4032
+  },
+  {
+    "url": "https://i.imgur.com/8LA4syM.jpg",
+    "width": 3024,
+    "height": 4032
+  },
+  {
+    "url": "https://i.imgur.com/rsAZZmR.jpg",
+    "width": 1440,
+    "height": 2712
+  },
+  {
+    "url": "https://i.imgur.com/65RLvXE.jpg",
+    "width": 3024,
+    "height": 4032
+  },
+  {
+    "url": "https://i.imgur.com/9fGeIpF.jpg",
+    "width": 3024,
+    "height": 4032
+  },
+  {
+    "url": "https://i.imgur.com/4MaIVxF.jpg",
+    "width": 3024,
+    "height": 4032
+  },
+  {
+    "url": "https://i.imgur.com/pVkUd8h.jpg",
+    "width": 720,
+    "height": 1278
+  },
+  {
+    "url": "https://i.imgur.com/azmW5K5.jpg",
+    "width": 722,
+    "height": 1280
+  },
+  {
+    "url": "https://i.imgur.com/IgT25Js.jpg",
+    "width": 722,
+    "height": 1280
+  },
+  {
+    "url": "https://i.imgur.com/ISaCRPR.jpg",
+    "width": 1020,
+    "height": 1920
+  },
+  {
+    "url": "https://i.imgur.com/bSDrs5g.jpg",
+    "width": 1020,
+    "height": 1920
+  },
+  {
+    "url": "https://i.imgur.com/V1iQjvN.jpg",
+    "width": 1428,
+    "height": 2688
+  },
+  {
+    "url": "https://i.imgur.com/ZFLgzgB.jpg",
     "width": 480,
-    "height": 640
+    "height": 800
   },
   {
-    "url": "https://uc033e83b7b8c1a7e90586dcc8cb.dl.dropboxusercontent.com/cd/0/inline/A9oIKU0NIfGrfiZY7VyPib-vhu0qN5qa3pt8jMBLRvcWqjjSIUEiLB8T3cOoSpl_lMMXCsKCu-tGIPxkAfB69KFdPg8VqY1jP-DRM6X6S-D0h19JPacpKn1AM9lQ9fo-VUY/file#",
-    "width": 300,
-    "height": 692
+    "url": "https://i.imgur.com/ovRxNlA.jpg",
+    "width": 1080,
+    "height": 1920
   },
   {
-    "url": "https://uc32d701720f3b7303244455f986.dl.dropboxusercontent.com/cd/0/inline/A9oofn8Gcty-4juBJLsKwGINAactQOAiHxTnda6nBE9r3ur4b5JHJVuWRYo853aWhBuhSHc3lscyf3a2stvPjbleExHi7gZOU594P0FAzdR6xbcjhyjhVTVaYAip0Zni5j4/file#",
-    "width": 389,
-    "height": 692
-  },
-  {
-    "url": "https://uc4a4b01bb04d5d254a2e9d9b07c.dl.dropboxusercontent.com/cd/0/inline/A9qMhemGN_mPPXZ3n0Av7rMkG8CDQ1RyWQD1hx59IHA7oD5OBRVrn661sJIkfD-n20g7hJq4YWnQNAyxDURilVjtXD4nqIcBmtDZfm2PgiI4ItNFaA7fiW_d5m9-qxVeBSU/file#",
-    "width": 389,
-    "height": 692
-  },
-  {
-    "url": "https://uc8d0e658eeea70c4b517ef98b59.dl.dropboxusercontent.com/cd/0/inline/A9qqN5_KS239IpIB7a_aOliApXC2xA_9d8UGnGS-U1VeBWlIGqjx1fEWt0GCcUpFto4S0kSZ238uuYd7OgTWUuB6GaMSZLa6CS5hQAE5VBUzVVN3Bndq4YvHSGsMyMGkiNM/file#",
-    "width": 519,
-    "height": 692
-  },
-  {
-    "url": "https://uc16f9610c665576d706a3a661b9.dl.dropboxusercontent.com/cd/0/inline/A9prbn7FYSWReCe-vW3JZKTXPXzoCsetzabQvgTUGBnjQTYMEoNvl7UAUyxmORYVBMA8p8xkAMrTq8qD5DHT5NyScGDHLYTKuiUcPT-Ufz3mwmGSDJDACisP4b7XGcS3lkM/file#",
-    "width": 367,
-    "height": 692
-  },
-  {
-    "url": "https://ucf41ae076dac13a0cdff220a38f.dl.dropboxusercontent.com/cd/0/inline/A9ojAuJZyRqIPnTY_g0FaTjRhDPs4esyT307vEQY78mD4HmjDXNuyIYKDexqbks8sJMmYNLTKgnVlIcZVODdfwsjZykB4TGgf5B6erTvLOgSlmoW7qIfxlZvHbrXq4FgJ8E/file#",
-    "width": 519,
-    "height": 692
-  },
-  {
-    "url": "https://uc2259276328ef506eddb90df435.dl.dropboxusercontent.com/cd/0/inline/A9rWtaF2lq58L3uB6u39APd0lMD80JuZoa7pBe_2d5T-aJ5z1AmBuyS7Cjfb7zN631mPxYO55ukmwUvvX2Am5W23lUiatLQg3R_R38WVEUKgyb3BDDuUZO8x9yildaNL6zM/file#",
-    "width": 367,
-    "height": 692
-  },
-  {
-    "url": "https://uc624d527ecc17a2e7665fbcf2fa.dl.dropboxusercontent.com/cd/0/inline/A9rUArfF_Idai39q_1mlEBkO3H-vuRtJ46pBmNP0Drz-ad_7OE937H_9SSQuvDi_f0nZTjReqZ4FBRYu7yisQwsp_Zd5KFIccvw0veVT0j5fqF5dylr6mHg6gqQTTzfOzhA/file#",
-    "width": 389,
-    "height": 692
-  },
-  {
-    "url": "https://ucaa644bf159b45ea04ae6a13d67.dl.dropboxusercontent.com/cd/0/inline/A9ptZ1SXEOlsmU47KyxlH_keuAOW3U_bdbfNwrmsZ5qvwkNe5mO_nXzYjcquVTrJAzd0_37gWpPyuw_uheUNzhH_C9c18oHbTWJ-Gw6hIaCaf41MpYshivdufvfbWNA0_X4/file#",
-    "width": 367,
-    "height": 692
-  },
-  {
-    "url": "https://uca0ea4e8565d9bd49e703af62da.dl.dropboxusercontent.com/cd/0/inline/A9rKdHHrSfHf3QGrQPHoO3erry5vvOM1jXHiD3tDrx88a8gPGWAvMbqt5ORm-Gls4R9lyDC6-upGiwuUGB7SIE_kDTDgcCd9jgLHkKmTxSL-d-z2P9r2iQuGogpY2bxcEmc/file#",
-    "width": 367,
-    "height": 692
-  },
-  {
-    "url": "https://uc3bfced979fbc1355b26e4c8413.dl.dropboxusercontent.com/cd/0/inline/A9qWvDwjTwMxzPN6w0psvw_2IjI9z3WFjkIgrrbS7hqNmXWjMkqZCus5oFT81jJjdh0uB5bqGyoQ0S-JqtrzDdKMAwUAQHUeVxVXakHK6RsvrGryR2zmNzC1IGBRU9IQVqs/file#",
-    "width": 367,
-    "height": 692
-  },
-  {
-    "url": "https://uc96f508b7bedd5d91dde6995500.dl.dropboxusercontent.com/cd/0/inline/A9r2CY3sHdkU99dDsRzjG1V6fjkblLbYj9ca91GIq5BCY2gPLH0JXdBKQsv85C1i8PjceSs3Tohsbr3fz3CcsbUTs0BRIU7vHSkmsP-_c80-bIetjPDO9xEF7VBhXIw9Q50/file#",
-    "width": 415,
-    "height": 692
+    "url": "https://i.imgur.com/B0e4FpG.jpg",
+    "width": 1428,
+    "height": 2688
   }
 ]
